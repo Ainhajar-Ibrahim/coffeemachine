02aa58d50e2c8f5de7911f004105c32d45b5bf46
PC PRO ULTRA RAPIDE <anassdah5@gmail.com>
2021-05-30 12:12:09 +0100

    while statement added

--- src/coffmach/Main.java
+++ src/coffmach/Main.java
@@ -1,12 +1,14 @@
 package coffmach;
 
-import java.sql.SQLOutput;
 import java.util.Scanner;
+
+import static java.lang.System.exit;
 
 public class Main implements fromuser,touser {
     private static int price;
     private static int money1;
-    static signal  message = new signal(100);
+    static signal message = new signal(100);
+
     public static int getNb() {
         return nb;
     }
@@ -25,79 +27,52 @@ public class Main implements fromuser,touser {
         Main.nbc = nbc;
     }
 
-    private static int nbc=0;
+    private static int nbc = 0;
+    static boolean restart = true;
 
 
     public static void main(String[] args) throws InterruptedException {
-        nb=touser.showMenu();
-        money1=fromuser.insertCoin();
+        while (restart) {
+            nb = touser.showMenu();
+            money1 = fromuser.insertCoin();
 
 
-        controller c = new controller(message);
-        hardware h = new hardware(message);
+            controller c = new controller(message);
+            hardware h = new hardware(message);
 
-            switch(nb){
-                case 1:{
+            switch (nb) {
+                case 1 -> {
 
 
-                    switch(money1){
-                        case 5:
-                            price=0;
+                    switch (money1) {
+                        case 5 -> {
+                            price = 0;
                             System.out.println("not enough money");
-                            touser.returnChange(money1,price);
+                            touser.returnChange(money1, price);
                             touser.showMenu();
-                            break;
-
-
-                        case 10:
-                            price=10;
-                            nbc=touser.showCoffeeMenu();
-
+                        }
+                        case 10 -> {
+                            price = 10;
+                            nbc = touser.showCoffeeMenu();
                             System.out.println("your coffee will be ready in no time");
                             h.start();
                             c.start();
-
-
-                            break;
-
-
-
-                        default:
-                            price=0;
+                        }
+                        default -> {
+                            price = 0;
                             System.out.println("you can only insert 5 or 10");
-                            touser.returnChange(money1,price);
+                            touser.returnChange(money1, price);
                             touser.showMenu();
-                            break;
-
+                        }
                     }
 
-                    break;}
+                }
+                case 2 -> {
 
-                case 2:
-                {
-
-                    switch(money1){
-
-
-                        case 5:
-                            price=5;
+                    switch (money1) {
+                        case 5 -> {
+                            price = 5;
                             System.out.println("your tea will be ready in no time");
-                            h.start();
-                            c.start();
-                            try {
-                                Thread.sleep(400); // In your case it would be: Thread.sleep(100);
-                            } catch (Exception e) {
-                                e.printStackTrace();
-                            }
-                           System.out.println("tea is ready!");
-                            break;
-
-
-
-                        case 10:
-                            price=5;
-                            System.out.println("your tea will be ready in no time");
-                            touser.returnChange(money1,price);
                             h.start();
                             c.start();
                             try {
@@ -106,30 +81,47 @@ public class Main implements fromuser,touser {
                                 e.printStackTrace();
                             }
                             System.out.println("tea is ready!");
-                            break;
-
-
-
-                        default:
-                            price=0;
+                        }
+                        case 10 -> {
+                            price = 5;
+                            System.out.println("your tea will be ready in no time");
+                            touser.returnChange(money1, price);
+                            h.start();
+                            c.start();
+                            try {
+                                Thread.sleep(400);
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                            System.out.println("tea is ready!");
+                        }
+                        default -> {
+                            price = 0;
                             System.out.println("you can only insert 5 or 10");
-                            touser.returnChange(money1,price);
+                            touser.returnChange(money1, price);
                             touser.showMenu();
-                            break;
+                        }
                     }
 
-                    break;}
-
-
-                default:
-                    System.out.println("unknown command");
-                    break;
+                }
+                default -> System.out.println("unknown command");
+            }
+            try {
+                Thread.sleep(500); // In your case it would be: Thread.sleep(100);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            System.out.println("Do you want something else?");
+            System.out.println("1..........Yes");
+            System.out.println("0..........No");
+            Scanner clavier=new Scanner(System.in);
+            int nb=clavier.nextInt();
+            if(nb==0){
+                System.out.println("Have a nice day");
+                exit(0); }
 
 
 
         }
-
-
-
     }
 }
