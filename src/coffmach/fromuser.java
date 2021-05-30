d401fcb30cfb2f6eb281b90232722e62c397c0e4
PC PRO ULTRA RAPIDE <anassdah5@gmail.com>
2021-05-30 19:02:34 +0100

    code cleaning

--- src/coffmach/fromuser.java
+++ src/coffmach/fromuser.java
@@ -3,7 +3,7 @@ package coffmach;
 import java.util.Scanner;
 
 public interface fromuser {
-    public static int insertCoin(){
+     static int insertCoin(){
         System.out.println("\n** Coffee Machine **\n");
         System.out.println("\n** Insert coins **\n");
         Scanner clavier=new Scanner(System.in);
